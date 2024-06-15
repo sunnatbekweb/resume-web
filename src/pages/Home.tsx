8aacec16d04d44ad8a02a951de67cc8666d0886a
sunnatbek07 <sunnatbekkadamov@gmail.com>
2024-06-15 06:42:06 +0500

    added font and updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ const Home = () => {
     <section className={styles.home}>
       <div className="container">
         <h1>Sunnatbek Qadamov</h1>
+        <h2>I'm a <span>Frontend developer</span> from Uzbekistan</h2>
       </div>
     </section>
   );
