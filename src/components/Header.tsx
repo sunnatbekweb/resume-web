3a6438a6c474ab61d93f42bc560b01c70fef187b
sunnatbekweb <sunnatbekkadamov@gmail.com>
2024-12-02 17:33:09 +0500

    changed module.css files to scc file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,25 @@
 import { Link, NavLink } from "react-router-dom";
-import styles from "../styles/Header.module.css";
+import "../styles/Header.scss";
 
 const Header = () => {
   const logoText = "<SQ/>";
   return (
-    <header className={styles.header}>
+    <header className="header">
       <div className="container">
-        <div className={styles.wrapper}>
-          <Link to={"/"} className={styles.logo}>
+        <div className="wrapper">
+          <Link to={"/"} className="logo">
             {logoText}
           </Link>
-          <div className={styles.header_right}>
-            <ul className={styles.nav_list}>
-              <li className={styles.nav_list_item}>
+          <div className="header_right">
+            <ul className="nav_list">
+              <li className="nav_list_item">
                 <NavLink to="/">Home</NavLink>
               </li>
-              <li className={styles.nav_list_item}>
+              <li className="nav_list_item">
                 <NavLink to="/about">About</NavLink>
               </li>
             </ul>
-            <button className={styles.download_button}>Download CV</button>
+            <button className="download_button">Download CV</button>
           </div>
         </div>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,37 @@
 import { FaGithub, FaLinkedin, FaTelegram } from "react-icons/fa";
-import styles from "../styles/Home.module.css";
+import "../styles/Home.scss";
 import { MdEmail } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <section className={styles.home}>
+    <section className="home">
       <div className="container">
         <h1>Sunnatbek Qadamov</h1>
         <h2>
           I'm a <span>Frontend developer</span> from Uzbekistan
         </h2>
-        <div className={styles.icons_container}>
+        <div className="icons_container">
           <Link
             to={"https://www.linkedin.com/in/sunnatbekqadamov/"}
             target="_blank"
           >
-            <div className={styles.icons_wrapper}>
+            <div className="icons_wrapper">
               <FaLinkedin />
             </div>
           </Link>
           <Link to={"https://github.com/sunnatbekweb"} target="_blank">
-            <div className={styles.icons_wrapper}>
+            <div className="icons_wrapper">
               <FaGithub />
             </div>
           </Link>
           <Link to={"https://t.me/Sunnatbek_Kadamov"} target="_blank">
-            <div className={styles.icons_wrapper}>
+            <div className="icons_wrapper">
               <FaTelegram />
             </div>
           </Link>
           <Link to={"mailto:sunnatbekkadamov@gmail.com"} target="_blank">
-            <div className={styles.icons_wrapper}>
+            <div className="icons_wrapper">
               <MdEmail />
             </div>
           </Link>
