c18d73ac0b1977918734fcd5e9332ef699ad946c
sunnatbekweb <sunnatbekkadamov@gmail.com>
2024-12-04 18:34:41 +0500

    updated about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,4 @@
+import { FaLocationDot } from "react-icons/fa6";
 import "../styles/about.scss";
 
 const About = () => {
@@ -10,9 +11,27 @@ const About = () => {
               <img src="/src/assets/images/image.jpg" alt="Avatar" />
             </div>
             <h3>Sunnatbek Qadamov</h3>
-            <div className="contact">
-              
+            <div className="about__row--col-location">
+              <FaLocationDot style={{ fontSize: 20 }} />
+              <span>Tashkent, Uzbekistan</span>
             </div>
+            <div className="about__row--col-skills">
+              <div className="skills__item">#HTML5</div>
+              <div className="skills__item">#CSS3</div>
+              <div className="skills__item">#SCSS</div>
+              <div className="skills__item">#JavaScript</div>
+              <div className="skills__item">#TypeScript</div>
+              <div className="skills__item">#React</div>
+              <div className="skills__item">#Redux</div>
+              <div className="skills__item">#ReduxToolkit</div>
+              <div className="skills__item">#git</div>
+            </div>
+          </div>
+          <div>
+            <h2>Frontend Developer</h2>
+          </div>
+          <div>
+            <h4>My stack</h4>
           </div>
         </div>
       </div>
