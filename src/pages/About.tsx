023429f6e29bc90e2fee7303786c5766d3efad45
sunnatbekweb <sunnatbekkadamov@gmail.com>
2024-12-04 18:06:49 +0500

    installed icon library

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,21 @@
+import "../styles/about.scss";
+
 const About = () => {
   return (
-    <section>
-      <div>About</div>
+    <section className="container">
+      <div className="about">
+        <div className="about__row">
+          <div className="about__row--col">
+            <div className="about__row--col-imageContainer">
+              <img src="/src/assets/images/image.jpg" alt="Avatar" />
+            </div>
+            <h3>Sunnatbek Qadamov</h3>
+            <div className="contact">
+              
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
