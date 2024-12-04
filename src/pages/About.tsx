962da0166588eb7edcf8c016c516adb3dc66db9c
sunnatbekweb <sunnatbekkadamov@gmail.com>
2024-12-04 19:44:08 +0500

    updated about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -10,7 +10,7 @@ const About = () => {
             <div className="about__row--col-imageContainer">
               <img src="/src/assets/images/image.jpg" alt="Avatar" />
             </div>
-            <h3>Sunnatbek Qadamov</h3>
+            <h3 className="about__row--col-name">Sunnatbek Qadamov</h3>
             <div className="about__row--col-location">
               <FaLocationDot style={{ fontSize: 20 }} />
               <span>Tashkent, Uzbekistan</span>
@@ -18,7 +18,7 @@ const About = () => {
             <div className="about__row--col-skills">
               <div className="skills__item">#HTML5</div>
               <div className="skills__item">#CSS3</div>
-              <div className="skills__item">#SCSS</div>
+              <div className="skills__item">#SASS/SCSS</div>
               <div className="skills__item">#JavaScript</div>
               <div className="skills__item">#TypeScript</div>
               <div className="skills__item">#React</div>
@@ -27,11 +27,19 @@ const About = () => {
               <div className="skills__item">#git</div>
             </div>
           </div>
-          <div>
-            <h2>Frontend Developer</h2>
+          <div className="about__row--col with-background">
+            <h3 className="about__row--col-title">Frontend Developer</h3>
+            <h4 className="secondary-title">About me</h4>
+            <p className="text">
+              Being a frontend developer, I have skills in JavaScript, React.js,
+              and Next.js. I ensure that I integrate the latest js knowledge
+              into every task I perform. I pay attention to details, being
+              passionate about creating a user-friendly website for this
+              purpose.
+            </p>
           </div>
-          <div>
-            <h4>My stack</h4>
+          <div className="about__row--col">
+            <h3 className="about__row--col--title">My stack</h3>
           </div>
         </div>
       </div>
